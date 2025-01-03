bce8ac0d21fa1d50b65454254c832afec4dcf9bd
hoon <danielhoon@naver.com>
2025-01-03 10:09:19 +0900

    제일 처음 안드로이드 스튜디오에서 커밋하기

---
+++ Tag/WebViewActivity.java
@@ -0,0 +1,42 @@
+package com.example.desk0014.Tag;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.desk0014.R;
+
+public class WebViewActivity extends AppCompatActivity {
+    //웹뷰엑티비티
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // 클래스시작 온크리에이트
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        // 레이아웃 지정
+
+        WebView webView = findViewById(R.id.web_view);
+        //웹뷰 레이아웃 지정
+        String url = getIntent().getStringExtra("url");
+        // url 설정하기 인텐트
+
+        if (url != null && !url.isEmpty()) {
+            // URL이 http로 시작하는 경우 https로 변경
+            if (url.startsWith("http://")) {
+                url = url.replaceFirst("http://", "https://");
+            } else if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                // URL이 http나 https로 시작하지 않으면 https를 추가
+                url = "https://" + url;
+            }
+        }
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+}
